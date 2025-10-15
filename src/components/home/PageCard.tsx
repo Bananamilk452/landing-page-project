afacd67189f84ddada07698ded88aabd97b104cf
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-15 17:29:43 +0900

    feat(home): PageCard 이미지 스타일 변경

--- src/components/home/PageCard.tsx
+++ src/components/home/PageCard.tsx
@@ -18,7 +18,9 @@ export function PageCard({
       to={to}
       className="grid h-62 w-84 cursor-pointer grid-rows-2 overflow-hidden rounded-lg border shadow-md"
     >
-      <img className="size-full object-cover" src={src} alt={alt} />
+      <div className="size-full bg-gray-200 p-4">
+        <img className="size-full object-contain" src={src} alt={alt} />
+      </div>
       <div className="flex flex-col gap-2 p-4">
         <h3 className="font-semibold">{title}</h3>
         <p className="text-sm">{description}</p>
