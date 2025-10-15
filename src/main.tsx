4638e60d4054dd8c6ddbd2868424545fad501edf
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-15 15:04:56 +0900

    chore: add react router

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,16 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 
-import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+
+import { Index } from "./pages";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
